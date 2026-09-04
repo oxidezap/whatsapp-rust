f44d589cdae9099bab50cc7767a98d920ecd34c5
João Lucas <jlucaso@hotmail.com>
2026-09-04 20:54:13 -0300

    test(sqlite): configure wasm timer harness

--- storages/sqlite-storage/tests/wasm_retry_timer.rs
+++ storages/sqlite-storage/tests/wasm_retry_timer.rs
@@ -1,10 +1,8 @@
 #![cfg(target_family = "wasm")]
 
 use js_sys::Date;
-use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
+use wasm_bindgen_test::wasm_bindgen_test;
 use whatsapp_rust_sqlite_storage::test_retry_backoff;
-
-wasm_bindgen_test_configure!(run_in_node);
 
 #[wasm_bindgen_test]
 async fn retry_backoff_uses_a_real_cancellable_wasm_timer() {
